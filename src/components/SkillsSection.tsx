b3b4cf1829ff3fcbe0d444c565db051d64499d5f
BogdanLi <li.bogdan44@gmail.com>
2024-12-18 12:58:24 +0500

    build ready fixes

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -4,22 +4,6 @@ import { useTranslation } from "react-i18next";
 import { Database } from "../lib/types/database.types";
 import supabaseClient from "../lib/supabase-client";
 
-const skills = [
-  "JavaScript",
-  "TypeScript",
-  "React.js",
-  "Next.js",
-  "Astro.js",
-  "Redux Toolkit",
-  "Zustand",
-  "TailwindCSS / TailwindUI",
-  "Material UI",
-  "shadcn/ui",
-  "three.js / react-three-fiber",
-  "Figma",
-  "Nest.js",
-];
-
 export default function SkillsSection() {
   const [skills, setSkills] = useState<
     null | Database["public"]["Tables"]["skills"]["Row"][]
